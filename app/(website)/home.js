0a15bd555ea2442ee27086d33e9452a1bea70ac6
Mahmudul-hasan700 <128229930+Mahmudul-hasan700@users.noreply.github.com>
2024-02-10 05:27:42 +0000

    Modify

--- app/(website)/home.js
+++ app/(website)/home.js
@@ -6,7 +6,7 @@ import PostList from "@/components/postlist";
 import Loading from "./loading";
 
 export default function HomePage({ posts }) {
-  const postsPerPage = 3;
+  const postsPerPage = 10;
   const [currentPage, setCurrentPage] = useState(1);
 
   const totalPosts = posts.length;
@@ -15,32 +15,61 @@ export default function HomePage({ posts }) {
   const paginate = pageNumber => setCurrentPage(pageNumber);
 
   const renderPageNumbers = () => {
-    const visiblePages = 3; // Number of page numbers to show
     const pageNumbers = [];
 
-    // Calculate the range of page numbers to display
-    let startPage = Math.max(1, currentPage - Math.floor(visiblePages / 2));
-    let endPage = Math.min(totalPages, startPage + visiblePages - 1);
+    // First page is always displayed
+    pageNumbers.push(renderPageButton(1));
 
-    // Adjust startPage and endPage if necessary
-    if (totalPages - endPage < Math.floor(visiblePages / 2)) {
-      startPage = Math.max(1, endPage - visiblePages + 1);
-    }
-
-    for (let i = startPage; i <= endPage; i++) {
+    if (currentPage > 3) {
       pageNumbers.push(
-        <button
-          key={i}
-          onClick={() => paginate(i)}
-          className={`inline-flex items-center justify-center rounded-md border border-gray-300 bg-white px-3 py-2 mr-2 ${currentPage === i ? 'opacity-50 cursor-not-allowed' : ''}`}
-          disabled={currentPage === i}
-        >
-          {i}
-        </button>
+        <span
+          key="ellipsis1"
+          className="mr-2 inline-flex items-center justify-center rounded-md bg-transparent text-black dark:text-slate-200 px-3 py-2">
+          ...
+        </span>
       );
     }
 
+    if (currentPage > 2) {
+      pageNumbers.push(renderPageButton(currentPage - 1));
+    }
+
+    if (currentPage >= 2 && currentPage !== totalPages) {
+      pageNumbers.push(renderPageButton(currentPage));
+    }
+
+    if (currentPage < totalPages - 1) {
+      pageNumbers.push(renderPageButton(currentPage + 1));
+    }
+
+    if (currentPage < totalPages - 2) {
+      pageNumbers.push(
+        <span
+          key="ellipsis2"
+          className="mr-2 inline-flex items-center justify-center rounded-md bg-transparent text-black dark:text-slate-200 px-3 py-2">
+          ...
+        </span>
+      );
+    }
+
+    // Last page is always displayed
+    if (totalPages !== 1) {
+      pageNumbers.push(renderPageButton(totalPages));
+    }
+
     return pageNumbers;
+  };
+
+  const renderPageButton = pageNumber => {
+    return (
+      <button
+        key={pageNumber}
+        onClick={() => paginate(pageNumber)}
+        className={`relative inline-flex items-center rounded-md px-3.5 py-2 mr-2 text-sm font-medium focus:z-20 disabled:pointer-events-none ${currentPage === pageNumber ? "cursor-not-allowed bg-blue-500 text-white" : "text-gray-700 dark:text-gray-300 bg-white dark:bg-gray-800 dark:border-gray-500 dark:hover:bg-gray-700 border border-gray-300 dark:border-gray-600"}`}
+        disabled={currentPage === pageNumber}>
+        {pageNumber}
+      </button>
+    );
   };
 
   // Logic to display posts for the current page
