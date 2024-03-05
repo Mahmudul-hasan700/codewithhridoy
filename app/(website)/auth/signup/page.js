d846b5d11216d6e9def812cca84bbc1cf8ec044b
MAHMUDUL HASAN <128229930+Mahmudul-hasan700@users.noreply.github.com>
2024-03-05 01:37:56 +0000

    Modify

--- app/(website)/auth/signup/page.js
+++ app/(website)/auth/signup/page.js
@@ -2,9 +2,10 @@
 import Link from "next/link";
 import { useState, useEffect } from "react";
 import { useForm } from "react-hook-form";
-import { useGoogleLogin, GoogleLogin } from "@react-oauth/google";
+import { useGoogleLogin } from "@react-oauth/google";
 import axios from "axios";
 import GoogleIcon from "@/components/google";
+import { EyeIcon } from "@heroicons/react/24/outline";
 
 export default function Signup() {
   const {
@@ -267,69 +268,55 @@ export default function Signup() {
                       />
                       {errors.email && (
                         <p className="mt-1 text-red-500">
-                          Password is required
+                          Email is required
                         </p>
                       )}
                     </div>
                     <div className="mb-4">
-                      <div className="relative">
-                      <label
-                        htmlFor="password"
-                        className="mb-2 block fill-current text-sm font-medium">
-                        Choose a Password
-                      </label>
-                      <input
-                        type={showPassword ? "text" : "password"}
-                        id="password"
-                        {...register("password", { required: true })}
-                        placeholder="Enter your password"
-                        className={`block w-full rounded-lg border border-gray-300 border-gray-300 bg-gray-50 bg-white p-4 text-sm text-gray-800 text-gray-900 ${
-                          errors.password
-                            ? "border-red-500"
-                            : "border-gray-300 dark:border-gray-600"
-                        } outline-none focus:border-blue-500 focus:border-indigo-500 focus:ring-indigo-500 dark:bg-gray-900 dark:text-slate-200 dark:focus:border-slate-300`}
-                      />                    
-                      <div className="absolute inset-y-0 right-0 top-6 flex items-center pr-3 text-sm leading-5">
-                        <button
-                          type="button"
-                          onClick={togglePasswordVisibility}>
-                          {showPassword ? (
-                            <svg
-                              xmlns="http://www.w3.org/2000/svg"
-                              fill="none"
-                              viewBox="0 0 24 24"
-                              strokeWidth={1.5}
-                              stroke="currentColor"
-                              className="h-5 w-5">
-                              <path
-                                strokeLinecap="round"
-                                strokeLinejoin="round"
-                                d="M3.98 8.223A10.477 10.477 0 0 0 1.934 12C3.226 16.338 7.244 19.5 12 19.5c.993 0 1.953-.138 2.863-.395M6.228 6.228A10.451 10.451 0 0 1 12 4.5c4.756 0 8.773 3.162 10.065 7.498a10.522 10.522 0 0 1-4.293 5.774M6.228 6.228 3 3m3.228 3.228 3.65 3.65m7.894 7.894L21 21m-3.228-3.228-3.65-3.65m0 0a3 3 0 1 0-4.243-4.243m4.242 4.242L9.88 9.88"
-                              />
-                            </svg>
-                          ) : (
-                            <svg
-                              xmlns="http://www.w3.org/2000/svg"
-                              fill="none"
-                              viewBox="0 0 24 24"
-                              strokeWidth={1.5}
-                              stroke="currentColor"
-                              className="h-5 w-5">
-                              <path
-                                strokeLinecap="round"
-                                strokeLinejoin="round"
-                                d="M2.036 12.322a1.012 1.012 0 0 1 0-.639C3.423 7.51 7.36 4.5 12 4.5c4.638 0 8.573 3.007 9.963 7.178.07.207.07.431 0 .639C20.577 16.49 16.64 19.5 12 19.5c-4.638 0-8.573-3.007-9.963-7.178Z"
-                              />
-                              <path
-                                strokeLinecap="round"
-                                strokeLinejoin="round"
-                                d="M15 12a3 3 0 1 1-6 0 3 3 0 0 1 6 0Z"
-                              />
-                            </svg>
-                          )}
-                        </button>
+                      <div className="relative overflow-hidden">
+                        <label
+                          htmlFor="password"
+                          className="mb-2 block fill-current text-sm font-medium">
+                          Choose a Password
+                        </label>
+                        <input
+                          type={showPassword ? "text" : "password"}
+                          id="password"
+                          {...register("password", {
+                            required: true
+                          })}
+                          placeholder="Enter your password"
+                          className={`block w-full rounded-lg border border-gray-300 border-gray-300 bg-gray-50 bg-white p-4 text-sm text-gray-800 text-gray-900 ${
+                            errors.password
+                              ? "border-red-500"
+                              : "border-gray-300 dark:border-gray-600"
+                          } overflow-hidden outline-none focus:border-blue-500 focus:border-indigo-500 focus:ring-indigo-500 dark:bg-gray-900 dark:text-slate-200 dark:focus:border-slate-300`}
+                        />
+                        <div>
+                          <button
+                            type="button"
+                            className="absolute inset-y-0 right-0 top-6 flex items-center overflow-hidden rounded-md px-3 text-sm"
+                            onClick={togglePasswordVisibility}>
+                            {showPassword ? (
+                              <svg
+                                xmlns="http://www.w3.org/2000/svg"
+                                fill="none"
+                                viewBox="0 0 24 24"
+                                strokeWidth={1.5}
+                                stroke="currentColor"
+                                className="h-5 w-5">
+                                <path
+                                  strokeLinecap="round"
+                                  strokeLinejoin="round"
+                                  d="M3.98 8.223A10.477 10.477 0 0 0 1.934 12C3.226 16.338 7.244 19.5 12 19.5c.993 0 1.953-.138 2.863-.395M6.228 6.228A10.451 10.451 0 0 1 12 4.5c4.756 0 8.773 3.162 10.065 7.498a10.522 10.522 0 0 1-4.293 5.774M6.228 6.228 3 3m3.228 3.228 3.65 3.65m7.894 7.894L21 21m-3.228-3.228-3.65-3.65m0 0a3 3 0 1 0-4.243-4.243m4.242 4.242L9.88 9.88"
+                                />
+                              </svg>
+                            ) : (
+                              <EyeIcon className="h-5 w-5" />
+                            )}
+                          </button>
+                        </div>
                       </div>
-                    </div>
                       {errors.password && (
                         <p className="mt-1 text-red-500">
                           Password is required
--- models/User.ts
+++ models/User.ts
@@ -4,15 +4,13 @@ import mongoose, { Document, Schema } from 'mongoose';
 interface User extends Document {
   name: string;
   email: string;
-  password: string;
-  profileUrl: string; 
+  password: string
 }
 
 const UserSchema: Schema = new Schema({
   name: { type: String, required: true },
   email: { type: String, required: true, unique: true },
   password: { type: String },
-  profileUrl: { type: String, default: '/R.png' },
 });
 
 export default mongoose.models.User || mongoose.model<User>('User', UserSchema);
