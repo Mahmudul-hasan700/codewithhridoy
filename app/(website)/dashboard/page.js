36c64148aeb4c549d387f4272afdcbfe9dc89c2e
MAHMUDUL HASAN <128229930+Mahmudul-hasan700@users.noreply.github.com>
2024-03-22 12:02:10 +0000

    Modify

--- app/(website)/dashboard/page.js
+++ app/(website)/dashboard/page.js
@@ -1,10 +1,11 @@
-// pages/dashboard.js
 "use client";
 import { useEffect, useState } from "react";
 import axios from "axios";
+import { useRouter } from "next/navigation";
 
 export default function Dashboard() {
   const [userData, setUserData] = useState(null);
+  const router = useRouter();
 
   useEffect(() => {
     const token = localStorage.getItem("token");
@@ -23,17 +24,20 @@ export default function Dashboard() {
           // Handle error
         });
     } else {
-      console.error("Token not found in local storage.");
+      console.error("Token not found in local storage. Redirecting to login...");
+      router.push("/auth/login"); // Redirect to login page
     }
   }, []);
 
   return (
-    <div>
+    <div className="min-h-screen bg-gray-100 flex flex-col items-center justify-center">
       {userData ? (
-        <div>
-          <h1>Welcome, {userData.name}</h1>
-          <p>Email: {userData.email}</p>
-          <img src={userData.profileUrl} />
+        <div className="max-w-md bg-white shadow-md rounded-lg overflow-hidden">
+          <div className="p-6">
+            <h1 className="text-3xl font-semibold mb-2">Welcome, {userData.name}</h1>
+            <p className="text-gray-600">Email: {userData.email}</p>
+            <img src={userData.profileUrl} alt="Profile Picture" className="mt-4 w-full rounded-lg" />
+          </div>
         </div>
       ) : (
         <p>Loading...</p>
