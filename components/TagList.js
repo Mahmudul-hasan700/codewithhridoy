91f523da3b23d6dcffe7599091f89aac4d13915f
MAHMUDUL HASAN <128229930+Mahmudul-hasan700@users.noreply.github.com>
2024-08-01 03:53:57 +0000

    Modify

--- components/TagList.js
+++ components/TagList.js
@@ -1,15 +1,16 @@
 import React from "react";
+import Link from "next/link";
 
 export default function TagList({ tags }) {
   return (
     <div className="my-4">
       {tags && tags.length > 0 && (
         <ul className="flex flex-wrap *:rounded-full *:border *:border-sky-100 *:bg-sky-50 *:px-2 *:py-0.5 *:dark:bg-gray-700">
-          {tags.map(tag => (
-            <li key={tag?._id} className="mb-2 mr-2 flex flex-wrap">
-              <a href={`/tag/${tag.slug?.current}`}>
+          {tags.map((tag, index) => (
+            <li key={tag?._id || `tag-${index}`} className="mb-2 mr-2 flex flex-wrap">
+              <Link href={`/tag/${tag.slug?.current}`}>
                 {tag?.name}
-              </a>
+              </Link>
             </li>
           ))}
         </ul>
